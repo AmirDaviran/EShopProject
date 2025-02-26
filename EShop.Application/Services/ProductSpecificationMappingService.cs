7fe8e91cb3887ff9d6b250efee340586056d35fb
AmirDaviran <amirhasandaviran@gmail.com>
2025-02-26 21:52:19 +0330

    Fix Git Ignore

---
+++ EShop.Application/Services/ProductSpecificationMappingService.cs
@@ -0,0 +1,82 @@
+﻿using EShop.Application.Interfaces;
+using EShop.Domain.Entities.ProductEntity.Mapping;
+using EShop.Domain.Interfaces;
+using EShop.Domain.ViewModels.Products.Product_Specification;
+using static EShop.Domain.Enums.ProductSpecificationMapping.ProductSpecificationMappingEnums;
+
+namespace EShop.Application.Services
+{
+    public class ProductSpecificationMappingService : IProductSpecificationMappingService
+    {
+        #region Fields
+
+        private readonly IProductSpecificationMappingRepository _mappingRepository;
+        private readonly IProductRepository _productRepository;
+        private readonly ISpecificationRepository _specificationRepository;
+
+        #endregion
+
+
+
+        public async Task<List<SpecificationProductViewModel>> GetByProductIdAsync(int productId)
+        {
+            var mappings = await _mappingRepository.GetByProductIdAsync(productId);
+            return mappings.Select(m => new SpecificationProductViewModel
+            {
+                SpecificationId = m.SpecificationId,
+                Value = m.Value
+            }).ToList();
+        }
+
+        public async Task<CreateProductSpecificationResult> CreateAsync(int productId, SpecificationProductViewModel model)
+        {
+            if (model == null || string.IsNullOrWhiteSpace(model.Value) || model.SpecificationId <= 0)
+                return CreateProductSpecificationResult.InvalidInput;
+
+            var product = await _productRepository.GetByIdAsync(productId);
+            var specification = await _specificationRepository.GetByIdAsync(model.SpecificationId);
+            if (product == null || specification == null)
+                return CreateProductSpecificationResult.NotFound;
+
+            var mapping = new ProductSpecificationMapping
+            {
+                ProductId = productId,
+                SpecificationId = model.SpecificationId,
+                Value = model.Value,
+                CreatedDate = DateTime.Now
+            };
+
+            await _mappingRepository.InsertAsync(mapping);
+            await _mappingRepository.SaveAsync();
+            return CreateProductSpecificationResult.Success;
+        }
+
+        public async Task<UpdateProductSpecificationResult> UpdateAsync(int mappingId, SpecificationProductViewModel model)
+        {
+            if (model == null || string.IsNullOrWhiteSpace(model.Value) || model.SpecificationId <= 0)
+                return UpdateProductSpecificationResult.InvalidInput;
+
+            var mapping = await _mappingRepository.GetByIdAsync(mappingId);
+            if (mapping == null)
+                return UpdateProductSpecificationResult.NotFound;
+
+            mapping.SpecificationId = model.SpecificationId;
+            mapping.Value = model.Value;
+
+            _mappingRepository.Update(mapping);
+            await _mappingRepository.SaveAsync();
+            return UpdateProductSpecificationResult.Success;
+        }
+
+        public async Task<DeleteProductSpecificationResult> DeleteAsync(int mappingId)
+        {
+            var mapping = await _mappingRepository.GetByIdAsync(mappingId);
+            if (mapping == null)
+                return DeleteProductSpecificationResult.NotFound;
+
+            await _mappingRepository.DeleteAsync(mappingId);
+            await _mappingRepository.SaveAsync();
+            return DeleteProductSpecificationResult.Success;
+        }
+    }
+}
---
+++ EShop.Application/Services/SpecificationService.cs
@@ -0,0 +1,115 @@
+﻿using EShop.Application.Interfaces;
+using EShop.Domain.Entities.ProductEntity;
+using EShop.Domain.Enums.ProductEnums;
+using EShop.Domain.Enums.SpecificationEnum;
+using EShop.Domain.Interfaces;
+using EShop.Domain.ViewModels.Products.Product;
+using EShop.Domain.ViewModels.Products.Specification;
+
+namespace EShop.Application.Services;
+
+public class SpecificationService(ISpecificationRepository _specificationRepository) : ISpecificationService
+{
+    #region Create
+
+    public async Task<CreateSpecificationResult> CreateAsync(CreateSpecificationViewModel model)
+    {
+        #region Validation
+
+        if (model == null || string.IsNullOrWhiteSpace(model.Name))
+        {
+            return CreateSpecificationResult.InvalidInput;
+        }
+
+        #endregion
+
+
+        Specification specification = new Specification()
+        {
+            CreatedDate = DateTime.Now,
+            Name = model.Name
+        };
+
+        await _specificationRepository.InsertAsync(specification);
+        await _specificationRepository.SaveAsync();
+
+        return CreateSpecificationResult.Success;
+    }
+
+    #endregion
+
+    #region Delete
+
+    public async Task<DeleteSpecificationResult> DeleteAsync(int id)
+    {
+        var spec = await _specificationRepository.GetByIdAsync(id);
+
+        if (spec == null || spec.IsDeleted)
+        {
+            return DeleteSpecificationResult.NotFound;
+        }
+
+        spec.IsDeleted = true;
+
+        _specificationRepository.Update(spec);
+        await _specificationRepository.SaveAsync();
+
+        return DeleteSpecificationResult.Success;
+    }
+
+    #endregion
+
+    #region Filter
+
+    public async Task<FilterSpecificationViewModel> FilterAsync(FilterSpecificationViewModel model)
+    {
+        return await _specificationRepository.FilterAsync(model);
+    }
+
+    #endregion
+
+    #region Update
+
+    public async Task<UpdateSpecificationResult> UpdateAsync(UpdateSpecificationViewModel model)
+    {
+        if (model == null || model.Id <= 0)
+        {
+            return UpdateSpecificationResult.InvalidInput;
+        }
+
+        var spec = await _specificationRepository.GetByIdAsync(model.Id);
+        if (spec == null)
+        {
+            return UpdateSpecificationResult.NotFound;
+        }
+
+        spec.Name = model.Name;
+
+        _specificationRepository.Update(spec);
+        await _specificationRepository.SaveAsync();
+
+        return UpdateSpecificationResult.Success;
+    }
+
+    #endregion
+
+    #region GetForUpdate
+
+    public async Task<UpdateSpecificationViewModel> GetForUpdateAsync(int id)
+    {
+        var spec = await _specificationRepository.GetByIdAsync(id);
+        if (spec == null)
+        {
+            return null;
+        }
+        else
+        {
+            return new UpdateSpecificationViewModel()
+            {
+                Id = spec.Id,
+                Name = spec.Name
+            };
+        }
+    }
+    #endregion
+}
